af82a3d1a493f40acf6ea7d3b88452831f75a1e1
vahid alvandi <va6893@gmail.com>
2022-10-14 10:50:48 +0330

    Update PSR4Namespace.php

--- src/PSR4/PSR4Namespace.php
+++ src/PSR4/PSR4Namespace.php
@@ -2,301 +2,153 @@
 
 namespace HaydenPierce\ClassFinder\PSR4;
 
-use HaydenPierce\ClassFinder\ClassFinder;
+use HaydenPierce\ClassFinder\AppConfig;
 use HaydenPierce\ClassFinder\Exception\ClassFinderException;
 
-class PSR4Namespace
+class PSR4NamespaceFactory
 {
-    /** @var string */
-    private $namespace;
+    /** @var AppConfig */
+    private $appConfig;
 
-    /** @var string[] */
-    private $directories;
-
-    /** @var PSR4Namespace[] */
-    private $directSubnamespaces;
-
-    /**
-     * @param string $namespace
-     * @param string[] $directories
-     */
-    public function __construct($namespace, $directories)
+    public function __construct(AppConfig $appConfig)
     {
-        $this->namespace = $namespace;
-        $this->directories = $directories;
-    }
-
-    /**
-     * @param string $namespace
-     * @return bool
-     */
-    public function knowsNamespace($namespace)
-    {
-        $numberOfSegments = count(explode('\\', $namespace));
-        $matchingSegments = $this->countMatchingNamespaceSegments($namespace);
-
-        if ($matchingSegments === 0) {
-            // Provided namespace doesn't map to anything registered.
-            return false;
-        } elseif ($numberOfSegments <= $matchingSegments) {
-            // This namespace is a superset of the provided namespace. Namespace is known.
-            return true;
-        } else {
-            // This namespace is a subset of the provided namespace. We must resolve the remaining segments to a directory.
-            $relativePath = substr($namespace, strlen($this->namespace));
-            foreach ($this->directories as $directory) {
-                $path = $this->normalizePath($directory, $relativePath);
-                if (is_dir($path)) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
-
-    /**
-     * Determines how many namespace segments match the internal namespace. This is useful because multiple namespaces
-     * may technically match a registered namespace root, but one of the matches may be a better match. Namespaces that
-     * match, but are not _the best_ match are incorrect matches. TestApp1\\ is **not** the best match when searching for
-     * namespace TestApp1\\Multi\\Foo if TestApp1\\Multi was explicitly registered.
-     *
-     * PSR4Namespace $a;
-     * $a->namespace = "TestApp1\\";
-     * $a->countMatchingNamespaceSegments("TestApp1\\Multi") -> 1, TestApp1 matches.
-     *
-     * PSR4Namespace $b;
-     * $b->namespace = "TestApp1\\Multi";
-     * $b->countMatchingNamespaceSegments("TestApp1\\Multi") -> 2, TestApp1\\Multi matches
-     *
-     * PSR4Namespace $c;
-     * $c->namespace = "HaydenPierce\\Foo\\Bar";
-     * $c->countMatchingNamespaceSegments("TestApp1\\Multi") -> 0, No matches.
-     *
-     * @param string $namespace
-     * @return int
-     */
-    public function countMatchingNamespaceSegments($namespace)
-    {
-        $namespaceFragments = explode('\\', $namespace);
-        $undefinedNamespaceFragments = array();
-
-        while($namespaceFragments) {
-            $possibleNamespace = implode('\\', $namespaceFragments) . '\\';
-
-            if(strpos($this->namespace, $possibleNamespace) !== false){
-                return count(explode('\\', $possibleNamespace)) - 1;
-            }
-
-            array_unshift($undefinedNamespaceFragments, array_pop($namespaceFragments));
-        }
-
-        return 0;
-    }
-
-    /**
-     * @param string $namespace
-     * @return bool
-     */
-    public function isAcceptableNamespace($namespace)
-    {
-        $namespaceSegments = count(explode('\\', $this->namespace)) - 1;
-        $matchingSegments = $this->countMatchingNamespaceSegments($namespace);
-        return $namespaceSegments === $matchingSegments;
-    }
-
-    /**
-     * @param string $namespace
-     * @return bool
-     */
-    public function isAcceptableNamespaceRecursiveMode($namespace)
-    {
-        // Remove prefix backslash (TODO: review if we do this eariler).
-        $namespace = ltrim($namespace, '\\');
-
-        return strpos($this->namespace, $namespace) === 0;
-    }
-
-    /**
-     * Used to identify subnamespaces.
-     *
-     * @return string[]
-     */
-    public function findDirectories()
-    {
-        $self = $this;
-        $directories = array_reduce($this->directories, function($carry, $directory) use ($self){
-            $path = $self->normalizePath($directory, '');
-            $realDirectory = realpath($path);
-            if ($realDirectory !== false) {
-                return array_merge($carry, array($realDirectory));
-            } else {
-                return $carry;
-            }
-        }, array());
-
-        $arraysOfClasses = array_map(function($directory) use ($self) {
-            $files = scandir($directory);
-            return array_map(function($file) use ($directory, $self) {
-                return $self->normalizePath($directory, $file);
-            }, $files);
-        }, $directories);
-
-        $potentialDirectories = array_reduce($arraysOfClasses, function($carry, $arrayOfClasses) {
-            return array_merge($carry, $arrayOfClasses);
-        }, array());
-
-        // Remove '.' and '..' directories
-        $potentialDirectories = array_filter($potentialDirectories, function($potentialDirectory) {
-            $segments = explode('/', $potentialDirectory);
-            $lastSegment = array_pop($segments);
-
-            return  $lastSegment !== '.' && $lastSegment !== '..';
-        });
-
-        $confirmedDirectories = array_filter($potentialDirectories, function($potentialDirectory) {
-            return is_dir($potentialDirectory);
-        });
-
-        return $confirmedDirectories;
-    }
-
-    /**
-     * @param string $namespace
-     * @param int $options
-     * @return string[]
-     */
-    public function findClasses($namespace, $options = ClassFinder::STANDARD_MODE)
-    {
-        $relativePath = substr($namespace, strlen($this->namespace));
-
-        $self = $this;
-        $directories = array_reduce($this->directories, function($carry, $directory) use ($relativePath, $self){
-            $path = $self->normalizePath($directory, $relativePath);
-            $realDirectory = realpath($path);
-            if ($realDirectory !== false) {
-                return array_merge($carry, array($realDirectory));
-            } else {
-                return $carry;
-            }
-        }, array());
-
-        $arraysOfClasses = array_map(function($directory) {
-            return scandir($directory);
-        }, $directories);
-
-        $potentialClassFiles = array_reduce($arraysOfClasses, function($carry, $arrayOfClasses) {
-            return array_merge($carry, $arrayOfClasses);
-        }, array());
-
-        $potentialClasses = array_map(function($file) use ($namespace){
-            return $namespace . '\\' . str_replace('.php', '', $file);
-        }, $potentialClassFiles);
-
-        if ($options == ClassFinder::RECURSIVE_MODE) {
-            return $this->getClassesFromListRecursively($namespace);
-        } else {
-            return array_filter($potentialClasses, function($potentialClass) {
-                if (function_exists($potentialClass)) {
-                    // For some reason calling class_exists() on a namespace'd function raises a Fatal Error (tested PHP 7.0.8)
-                    // Example: DeepCopy\deep_copy
-                    return false;
-                } else {
-                    return class_exists($potentialClass);
-                }
-            });
-        }
-    }
-
-    /**
-     * @return string[]
-     */
-    private function getDirectClassesOnly()
-    {
-        $self = $this;
-        $directories = array_reduce($this->directories, function($carry, $directory) use ($self){
-            $path = $self->normalizePath($directory, '');
-            $realDirectory = realpath($path);
-            if ($realDirectory !== false) {
-                return array_merge($carry, array($realDirectory));
-            } else {
-                return $carry;
-            }
-        }, array());
-
-        $arraysOfClasses = array_map(function($directory) {
-            return scandir($directory);
-        }, $directories);
-
-        $potentialClassFiles = array_reduce($arraysOfClasses, function($carry, $arrayOfClasses) {
-            return array_merge($carry, $arrayOfClasses);
-        }, array());
-
-        $selfNamespace = $this->namespace; // PHP 5.3 BC
-        $potentialClasses = array_map(function($file) use ($selfNamespace) {
-            return $selfNamespace . str_replace('.php', '', $file);
-        }, $potentialClassFiles);
-
-        return array_filter($potentialClasses, function($potentialClass) {
-            if (function_exists($potentialClass)) {
-                // For some reason calling class_exists() on a namespace'd function raises a Fatal Error (tested PHP 7.0.8)
-                // Example: DeepCopy\deep_copy
-                return false;
-            } else {
-                return class_exists($potentialClass);
-            }
-        });
-    }
-
-    /**
-     * @param string $namespace
-     * @return string[]
-     */
-    public function getClassesFromListRecursively($namespace)
-    {
-        $initialClasses = strpos( $this->namespace, $namespace) !== false ? $this->getDirectClassesOnly() : array();
-
-        return array_reduce($this->getDirectSubnamespaces(), function($carry, PSR4Namespace $subNamespace) use ($namespace) {
-            return array_merge($carry, $subNamespace->getClassesFromListRecursively($namespace));
-        }, $initialClasses);
-    }
-
-    /**
-     * Join an absolute path and a relative path in a platform agnostic way.
-     *
-     * This method is also extracted so that it can be turned into a vfs:// stream URL for unit testing.
-     *
-     * @param string $directory
-     * @param string $relativePath
-     * @return mixed
-     */
-    public function normalizePath($directory, $relativePath)
-    {
-        $path = str_replace('\\', '/', $directory . '/' . $relativePath);
-        return $path;
+        $this->appConfig = $appConfig;
     }
 
     /**
      * @return PSR4Namespace[]
      */
-    public function getDirectSubnamespaces()
+    public function getPSR4Namespaces()
     {
-        return $this->directSubnamespaces;
+        $namespaces = $this->getUserDefinedPSR4Namespaces();
+        $vendorNamespaces = require(base_path() . '/vendor/composer/autoload_psr4.php');
+
+        $namespaces = array_merge($vendorNamespaces, $namespaces);
+
+        // There's some wackiness going on here for PHP 5.3 compatibility.
+        $names = array_keys($namespaces);
+        $directories = array_values($namespaces);
+        $self = $this;
+        $namespaces = array_map(function ($index) use ($self, $names, $directories) {
+            return $self->createNamespace($names[$index], $directories[$index]);
+        }, range(0, count($namespaces) - 1));
+
+        return $namespaces;
     }
 
     /**
-     * @param PSR4Namespace[] $directSubnamespaces
+     * @return string[]
      */
-    public function setDirectSubnamespaces($directSubnamespaces)
+    private function getUserDefinedPSR4Namespaces()
     {
-        $this->directSubnamespaces = $directSubnamespaces;
+        $appRoot = $this->appConfig->getAppRoot();
+
+        $composerJsonPath = $appRoot . 'composer.json';
+        $composerConfig = json_decode(file_get_contents($composerJsonPath));
+
+        if (!isset($composerConfig->autoload)) {
+            return array();
+        }
+
+        //Apparently PHP doesn't like hyphens, so we use variable variables instead.
+        $psr4 = "psr-4";
+        return (array)$composerConfig->autoload->$psr4;
     }
 
     /**
-     * @return mixed
+     * Creates a namespace from composer_psr4.php and composer.json autoload.psr4 items.
+     *
+     * @param string $namespace
+     * @param string[] $directories
+     * @return PSR4Namespace
+     * @throws ClassFinderException
      */
-    public function getNamespace()
+    public function createNamespace($namespace, $directories)
     {
-        return trim($this->namespace, '\\');
+        if (is_string($directories)) {
+            // This is an acceptable format according to composer.json
+            $directories = array($directories);
+        } elseif (is_array($directories)) {
+            // composer_psr4.php seems to put everything in this format
+        } else {
+            throw new ClassFinderException('Unknown PSR4 definition.');
+        }
+
+        $self = $this;
+        $appConfig = $this->appConfig;
+        $directories = array_map(function ($directory) use ($self, $appConfig) {
+            if ($self->isAbsolutePath($directory)) {
+                return $directory;
+            } else {
+                return $appConfig->getAppRoot() . $directory;
+            }
+        }, $directories);
+
+        $directories = array_filter(array_map(function ($directory) {
+            return realpath($directory);
+        }, $directories));
+
+        $psr4Namespace = new PSR4Namespace($namespace, $directories);
+
+        $subNamespaces = $this->getSubnamespaces($psr4Namespace);
+        $psr4Namespace->setDirectSubnamespaces($subNamespaces);
+
+        return $psr4Namespace;
+    }
+
+    /**
+     * @param PSR4Namespace $psr4Namespace
+     * @return PSR4Namespace[]
+     */
+    private function getSubnamespaces(PSR4Namespace $psr4Namespace)
+    {
+        // Scan it's own directories.
+        $directories = $psr4Namespace->findDirectories();
+
+        $self = $this;
+        $subnamespaces = array_map(function ($directory) use ($self, $psr4Namespace) {
+            $segments = explode('/', $directory);
+            $subnamespaceSegment = array_pop($segments);
+
+            $namespace = $psr4Namespace->getNamespace() . "\\" . $subnamespaceSegment . "\\";
+            return $self->createNamespace($namespace, $directory);
+        }, $directories);
+
+        return $subnamespaces;
+    }
+
+    /**
+     * Check if a path is absolute.
+     *
+     * Mostly this answer https://stackoverflow.com/a/38022806/3000068
+     * A few changes: Changed exceptions to be ClassFinderExceptions, removed some ctype dependencies,
+     * updated the root prefix regex to handle Window paths better.
+     *
+     * @param string $path
+     * @return bool
+     * @throws ClassFinderException
+     */
+    public function isAbsolutePath($path)
+    {
+        if (!is_string($path)) {
+            $mess = sprintf('String expected but was given %s', gettype($path));
+            throw new ClassFinderException($mess);
+        }
+
+        // Optional wrapper(s).
+        $regExp = '%^(?<wrappers>(?:[[:print:]]{2,}://)*)';
+        // Optional root prefix.
+        $regExp .= '(?<root>(?:[[:alpha:]]:[/\\\\]|/)?)';
+        // Actual path.
+        $regExp .= '(?<path>(?:[[:print:]]*))$%';
+        $parts = array();
+        if (!preg_match($regExp, $path, $parts)) {
+            $mess = sprintf('Path is NOT valid, was given %s', $path);
+            throw new ClassFinderException($mess);
+        }
+        if ('' !== $parts['root']) {
+            return true;
+        }
+        return false;
     }
 }
